a109f981d204ff1863f34bc54055828a562b2e9f
Veer <veerksheth@gmail.com>
2026-06-26 14:11:22 +1000

    add toggle switch widget

--- common.py
+++ common.py
@@ -1,6 +1,6 @@
 import subprocess
-from PySide6.QtCore import QEvent, Qt
-from PySide6.QtGui import QKeyEvent
+from PySide6.QtCore import QEvent, Qt, Signal
+from PySide6.QtGui import QColor, QKeyEvent, QPainter
 from PySide6.QtWidgets import QFrame, QHBoxLayout, QListWidget, QWidget
 
 
@@ -30,6 +30,44 @@ def make_centered(host, max_width=1100):
     outer.addWidget(inner, stretch=1)
     outer.addStretch()
     return inner
+
+
+class ToggleSwitch(QWidget):
+    toggled = Signal(bool)
+
+    _W, _H = 48, 24
+
+    def __init__(self):
+        super().__init__()
+        self._on = False
+        self.setFixedSize(self._W, self._H)
+        self.setCursor(Qt.PointingHandCursor)
+
+    def set_on(self, on, silent=False):
+        self._on = on
+        self.update()
+        if not silent:
+            self.toggled.emit(on)
+
+    def is_on(self):
+        return self._on
+
+    def mousePressEvent(self, e):
+        if e.button() == Qt.LeftButton:
+            self.set_on(not self._on)
+
+    def paintEvent(self, _):
+        p = QPainter(self)
+        p.setRenderHint(QPainter.Antialiasing)
+        w, h = self._W, self._H
+        p.setPen(Qt.NoPen)
+        p.setBrush(QColor("#2e2e2e"))
+        p.drawRoundedRect(0, 0, w, h, h / 2, h / 2)
+        m = 3
+        d = h - 2 * m
+        p.setBrush(QColor("#d8d8d8"))
+        p.drawEllipse((w - m - d) if self._on else m, m, d, d)
+        p.end()
 
 
 class NavList(QListWidget):
