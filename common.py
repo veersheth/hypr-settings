6ad938d81c1a0986233a035c742189a8b750984f
Veer <veerksheth@gmail.com>
2026-06-26 14:12:16 +1000

    animate switch widget

--- common.py
+++ common.py
@@ -1,5 +1,5 @@
 import subprocess
-from PySide6.QtCore import QEvent, Qt, Signal
+from PySide6.QtCore import QEasingCurve, QEvent, Property, QPropertyAnimation, Qt, Signal
 from PySide6.QtGui import QColor, QKeyEvent, QPainter
 from PySide6.QtWidgets import QFrame, QHBoxLayout, QListWidget, QWidget
 
@@ -40,12 +40,29 @@ class ToggleSwitch(QWidget):
     def __init__(self):
         super().__init__()
         self._on = False
+        self._pos = 0.0  # 0.0 = left (off), 1.0 = right (on)
         self.setFixedSize(self._W, self._H)
         self.setCursor(Qt.PointingHandCursor)
 
+        self._anim = QPropertyAnimation(self, b"handle_pos", self)
+        self._anim.setDuration(150)
+        self._anim.setEasingCurve(QEasingCurve.InOutQuad)
+
+    def _get_pos(self):
+        return self._pos
+
+    def _set_pos(self, val):
+        self._pos = val
+        self.update()
+
+    handle_pos = Property(float, _get_pos, _set_pos)
+
     def set_on(self, on, silent=False):
         self._on = on
-        self.update()
+        self._anim.stop()
+        self._anim.setStartValue(self._pos)
+        self._anim.setEndValue(1.0 if on else 0.0)
+        self._anim.start()
         if not silent:
             self.toggled.emit(on)
 
@@ -65,8 +82,9 @@ class ToggleSwitch(QWidget):
         p.drawRoundedRect(0, 0, w, h, h / 2, h / 2)
         m = 3
         d = h - 2 * m
+        hx = int(m + self._pos * (w - 2 * m - d))
         p.setBrush(QColor("#d8d8d8"))
-        p.drawEllipse((w - m - d) if self._on else m, m, d, d)
+        p.drawEllipse(hx, m, d, d)
         p.end()
 
 
